06093b39fac2b785da2a355e0cec0eff8c0d72e6
paulvinzent <manalopv@gmail.com>
2023-10-01 03:18:25 +0800

    sign in

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,9 +3,72 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class UserController extends BaseController
 {
+   public function register()
+   {
+    helper (['form']);
+    $rules = [
+        'username' => 'required|min_length(4)|max_length[100]valid_email|is_unique[users. email]',
+        'password' => 'required|min_length(4)|max_length[50]',
+        'confirmpassword' => 'matches[password]'
+    ];
+
+    if($this->validate($rules)){
+        $userModel = new UserModel();
+        $data = [
+            'username' => $this->request->getVar('username'),
+            'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+        ];
+
+    $userModel->save($data);
+    return redirect() -> to('/signin');
+    }else{
+    $data['validation'] = $this->validator;
+    echo view('signup', $data);
+    }
+   }
+
+   public function Login()
+   {
+    helper(['form']);
+    echo view('signin');
+   }
+
+   public function LoginAuth()
+   {
+    $session = session();
+    $userModeI = new UserModel();
+    $username = $this->request->getVar('username');
+    $password = $this->request->getVar('password');
+
+    $data = $userModel->where(username, $username)->first();
+
+    if($data){
+        $pass = $data['password'];
+        $authenticatePassword = password_verify($password, $pass);
+        if($authenticatePassword){
+        $ses_data = [
+        'id' => $data['id'],
+        username=> $data['username'],
+        'isLoggedln' => TRUE
+        ];
+
+        $session->set($ses_data);
+        return redirect()->to('/profile');
+
+    }else{
+        $session->setFlashdata('msg', 'Password is incorrect.');
+        return redirect()->to('/signin');
+    }
+    }else{
+        $session->setFlashdata('msg', 'Email does not exist. ');
+        return redirect()->to('/signin');
+    }
+}
+
     public function index()
     {
         //
